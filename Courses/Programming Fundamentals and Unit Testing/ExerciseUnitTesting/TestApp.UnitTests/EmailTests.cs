f9fb337feef2112b04af3da9770fe1f44abeb623
Alex93777 <130772195+Alex93777@users.noreply.github.com>
2024-12-29 18:39:10 +0200

    ExerciseUnitTesting

---
+++ Courses/Programming Fundamentals and Unit Testing/ExerciseUnitTesting/TestApp.UnitTests/EmailTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string email = "test@example.com";
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(isValidEmail, Is.EqualTo(true));
+
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string email = "test.example.com";
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(isValidEmail, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string email = null;
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(isValidEmail, Is.EqualTo(false));
+    }
+    [Test]
+    public void Test_IsValidEmail_WideSpaceEmail()
+    {
+        // Arrange
+        string email = "    ";
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(isValidEmail, Is.EqualTo(false));
+    }
+}
---
+++ Courses/Programming Fundamentals and Unit Testing/ExerciseUnitTesting/TestApp.UnitTests/PatternTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_SortInPattern_NullInput_ThrowsException()
+    {
+        // Arrange
+        int[]? inputArray = null;
+
+        // Act + Assert
+        Assert.That(() => Pattern.SortInPattern(inputArray), Throws.ArgumentException);
+
+    }
+
+    [Test]
+    public void Test_SortInPattern_SortsIntArrayInPattern_SortsCorrectly()
+    {
+        // Arrange
+        int[] inputArray = new int[] { 1, 2, 1, 3, 4, 10, 12, 15 };
+
+        // Act
+        int[] result = Pattern.SortInPattern(inputArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[]{ 1, 15, 2, 12, 3, 10, 4}));
+    }
+
+    [Test]
+    public void Test_SortInPattern_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        int[] empthyArray = Array.Empty<int>();
+
+        // Act
+        int[] result = Pattern.SortInPattern(empthyArray);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_SortInPattern_SingleElementArray_ReturnsSameArray()
+    {
+        // Arrange
+        int[] empthyArray = new int[] { 5 };
+
+        // Act
+        int[] result = Pattern.SortInPattern(empthyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(empthyArray));
+    }
+}
